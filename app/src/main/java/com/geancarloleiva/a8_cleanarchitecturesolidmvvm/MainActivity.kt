7fefc5501d6853c18b7fb801d3db186ef6cf7a2a
geanclv <geanclv@gmail.com>
2022-07-13 13:00:35 -0500

    Initial commit

---
+++ app/src/main/java/com/geancarloleiva/a8_cleanarchitecturesolidmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.geancarloleiva.a8_cleanarchitecturesolidmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
