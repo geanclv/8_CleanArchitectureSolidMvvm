3de19e5a9ffabdfb70d1c6928ebfa8062da3a81c
geanclv <geanclv@gmail.com>
2022-07-13 14:20:45 -0500

    Structuring the project (packages) and creating a new Module: 1. app: for Android components (framework and presentation packages); 2. core: for non Android components (data, repository, usecase packages).

--- app/src/main/java/com/geancarloleiva/a8_cleanarchitecturesolidmvvm/presentation/MainActivity.kt
+++ app/src/main/java/com/geancarloleiva/a8_cleanarchitecturesolidmvvm/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.geancarloleiva.a8_cleanarchitecturesolidmvvm
+package com.geancarloleiva.a8_cleanarchitecturesolidmvvm.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geancarloleiva.a8_cleanarchitecturesolidmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
