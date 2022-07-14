81bd95ce435ae3bf7339f2c56b425fc97ad99878
geanclv <geanclv@gmail.com>
2022-07-14 10:43:54 -0500

    Creating Entity class Note.

---
+++ app/src/main/java/com/geancarloleiva/a8_cleanarchitecturesolidmvvm/framework/db/NoteEntity.kt
@@ -0,0 +1,29 @@
+package com.geancarloleiva.a8_cleanarchitecturesolidmvvm.framework.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.geancarloleiva.core.data.Note
+
+@Entity(tableName = "note")
+data class NoteEntity (
+    var title: String,
+    var content: String,
+    @ColumnInfo(name = "creation_date")
+    var creationTime: Long,
+    @ColumnInfo(name = "update_date")
+    var updateTime: Long,
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L
+) {
+
+    companion object {
+        fun fromNote(note: Note){
+            NoteEntity(note.title, note.content, note.creationTime, note.updateTime)
+        }
+    }
+
+    fun toNote(){
+        Note(title, content, creationTime, updateTime, id)
+    }
+}
