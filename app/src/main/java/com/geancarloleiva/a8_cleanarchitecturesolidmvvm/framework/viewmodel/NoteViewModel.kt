b1e11527daf7f4ed1981a2859dea7bf86dbb7dd9
geanclv <geanclv@gmail.com>
2022-07-14 11:38:44 -0500

    Implementing de ViewModel for save Note.

---
+++ app/src/main/java/com/geancarloleiva/a8_cleanarchitecturesolidmvvm/framework/viewmodel/NoteViewModel.kt
@@ -0,0 +1,41 @@
+package com.geancarloleiva.a8_cleanarchitecturesolidmvvm.framework.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.geancarloleiva.a8_cleanarchitecturesolidmvvm.framework.RoomNoteDataSource
+import com.geancarloleiva.a8_cleanarchitecturesolidmvvm.framework.UseCases
+import com.geancarloleiva.core.data.Note
+import com.geancarloleiva.core.repository.NoteRepository
+import com.geancarloleiva.core.usecase.AddNote
+import com.geancarloleiva.core.usecase.GetAllNotes
+import com.geancarloleiva.core.usecase.GetNote
+import com.geancarloleiva.core.usecase.RemoveNote
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application): AndroidViewModel(application) {
+
+    //Communicating with Room BD in background using Coroutines
+    //Dispatchers.IO is more efficient for communicate with remote dataSources (web, db, etc)
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    private val repository = NoteRepository(RoomNoteDataSource(application))
+
+    private val useCases = UseCases(
+        AddNote(repository),
+        GetNote(repository),
+        GetAllNotes(repository),
+        RemoveNote(repository)
+    )
+
+    private val saved = MutableLiveData<Boolean>()
+
+    fun saveNote(note: Note){
+        coroutineScope.launch {
+            useCases.addNote(note)
+            saved.value = true
+        }
+    }
+}
